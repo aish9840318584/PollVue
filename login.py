44cf7976d1b024464ee99a135a38d3352d9c28a9
aish9840318584 <130387672+aish9840318584@users.noreply.github.com>
2024-06-30 21:10:33 +0530

    Update login.py

--- login.py
+++ login.py
@@ -3,7 +3,7 @@
 import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, auth
-# Initialize Firebase app (only once)
+
 if not firebase_admin._apps:
     cred = credentials.Certificate("./serviceAccountKey.json")
     firebase_admin.initialize_app(cred)
